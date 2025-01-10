ecb3cc78b3c7dec8df40441b2bd7158c6888c88a
Leif <squall140@gmail.com>
2025-01-10 11:04:41 +0800

    feat: introduce prettier config and add a simple demo of useState

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,9 @@ module.exports = {
     },
     extends: [
       'eslint:recommended',
+      'plugin:react/recommended',
       'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
